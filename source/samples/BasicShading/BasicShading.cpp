ff376a9a6e11f8edde9577bb51b9bce0a9b1745d
bhlzlx <bhlzlx@gmail.com>
2019-06-24 21:01:24 +0800

    添加新工程

---
+++ source/samples/BasicShading/BasicShading.cpp
@@ -0,0 +1,387 @@
+#include <NixApplication.h>
+#include <rapidjson/rapidjson.h>
+#include <rapidjson/document.h>
+#include <rapidjson/stringbuffer.h>
+#include <rapidjson/writer.h>
+#include <NixJpDecl.h>
+#include <NixRenderer.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <tiny_obj_loader.h>
+
+#include <nix/io/archieve.h>
+#include <cstdio>
+#include <cassert>
+
+#ifdef _WIN32
+    #include <Windows.h>
+#endif
+
+namespace Nix {
+
+	struct CVertex {
+		glm::vec3 xyz;
+		glm::vec3 norm;
+		glm::vec2 coord;
+		glm::vec3 tangent;
+	};
+
+	glm::vec3 calTangent(const glm::vec3& _pdir1, glm::vec3& _pdir2, const glm::vec3& _uvdir1, glm::vec3& _uvdir2) {
+		// assume T,B,N
+		// pdir.xyz =		uv.x	[Tx, Bx, Nx]
+		//				    uv.y *	[Ty, By, Ny]
+		//				    uv.z	[Tz, Bz, Nz]
+
+		// or
+
+		// pdir.xyz =		[Tx, Ty, Tz]   uv.x,y,z
+		//				    [Bx, By, Bz] *  
+		//				    [Nx, Ny, Nz]   	
+
+		// 
+		// pdir.x = Tx * uv.x + Bx * uv.y;
+		// pdir.y = Ty * uv.y + By * uv.y;
+		// pdir.z = Tz * uv.x + Bz * uv.y;
+		// 
+		// pdir1 = uv1.x * T + uv1.y * B;
+		// pdir2 = uv2.x * T + uv2.y * B;
+
+		// pdir1 * uv2.y = uv1.x * uv2.y * T + uv1.y * uv2.y * B;
+		// pdir2 * uv1.y = uv2.x * uv1.y * T + uv1.y * uv2.y * B;
+
+		// pdir1*uv2.y - pdir2 * uv1.y = (uv1.x * uv2.y - uv2.x * uv1.y) * T
+
+		// T = ( pdir1 * uv2.y - pdir2 * uv1.y ) / (uv1.x * uv2.y - uv2.x * uv1.y )
+
+		glm::vec3 tangent = (_pdir1 * _uvdir2.y - _pdir2 * _uvdir1.y) / (_uvdir1.x * _uvdir2.y - _uvdir2.x * _uvdir1.y);
+		return tangent;
+	}
+
+	struct Config {
+		float eye[3];
+		float lookat[3];
+		float up[3];
+		float light[3];
+		NIX_JSON( eye, lookat, up, light )
+	};
+
+	void CreateSphere(int _nstack, int _nslice, std::vector<CVertex>& _vertices, std::vector<uint16_t>& _indices) {
+		assert(_nstack >= 3);
+		assert(_nslice >= 3);
+		//
+		float dy = 2.0f / _nstack;
+		int mid = (_nstack - 1) / 2;
+		int midSliceCount = mid + _nslice;
+
+		std::vector< CVertex > vertices;
+
+		for (int stk = 0; stk < _nstack; ++stk) {
+			int sliceCount = 0;
+			if (_nstack & 0x1) { // ÆæÊý
+				sliceCount = midSliceCount - abs(mid - stk);
+			}
+			else {
+				if (stk > mid) {
+					sliceCount = midSliceCount - abs(mid - stk + 1);
+				}
+				else {
+					sliceCount = midSliceCount - abs(mid - stk);
+				}
+			}
+			float yarc = 3.141592654f / (_nstack - 1) * stk;
+			float y = cos(yarc);
+			float xzref = sin(yarc);
+			float x = 0;
+			float z = 0;
+			float dxarc = 3.141592654f * 2.0f / (sliceCount - 1);
+			for (int slc = 0; slc < sliceCount; ++slc) {
+				float xarc = dxarc * slc;
+				x = cos(xarc) * xzref;
+				z = sin(xarc) * xzref;
+				//
+				glm::vec3 pos(x, y, z);
+				glm::vec3 normal(x, y, z);
+				glm::vec2 coord(1.0f - 1.0f / (sliceCount - 1) * slc, 1.0f - (y + 1.0f) / 2.0f);
+				//
+				CVertex vertex;
+				vertex.xyz = pos;
+				vertex.norm = normal;
+				vertex.coord = coord;
+				//
+				_vertices.push_back(vertex);
+			}
+			//printf("stack : %d , slice : %d", stk, sliceCount);
+		}
+		// create indices
+		size_t count = _vertices.size();
+		size_t counter = 0;
+		if (_nstack & 0x1) { // ÆæÊý
+			for (int stk = 0; stk < mid; ++stk) {
+				int sliceCount = midSliceCount - abs(mid - stk);
+				for (size_t ct = 1; ct <= sliceCount; ++ct)
+				{
+					uint16_t sixple[6];
+					sixple[0] = counter;
+					sixple[1] = counter + sliceCount;
+					sixple[2] = sixple[1] + 1;
+					//
+					sixple[3] = (count - sixple[0]) - 1;
+					sixple[4] = (count - sixple[1]) - 1;
+					sixple[5] = (count - sixple[2]) - 1;
+					//
+					for (auto index : sixple)
+						_indices.push_back(index);
+					if (stk != 0 && ct != sliceCount) {
+						sixple[0] = counter;
+						sixple[1] = counter + sliceCount + 1;
+						sixple[2] = counter + 1;
+
+						sixple[3] = (count - sixple[0]) - 1;
+						sixple[4] = (count - sixple[1]) - 1;
+						sixple[5] = (count - sixple[2]) - 1;
+
+
+						//sixple[3] = count - sixple[0] - 1; sixple[4] = count - sixple[1] - 1; sixple[5] = sixple[4] - 1;
+						for (auto index : sixple)
+							_indices.push_back(index);
+					}
+					//
+					++counter;
+				}
+			}
+		}
+		else { // Å¼Êý
+			for (int stk = 0; stk < mid; ++stk) {
+				int sliceCount = 0;
+				if (stk > mid) {
+					sliceCount = midSliceCount - abs(mid - stk + 1);
+				}
+				else {
+					sliceCount = midSliceCount - abs(mid - stk);
+				}
+				//
+				for (size_t ct = 1; ct <= sliceCount; ++ct)
+				{
+					uint16_t sixple[6];
+					sixple[0] = counter;
+					sixple[1] = counter + sliceCount;
+					sixple[2] = sixple[1] + 1;
+					//
+					sixple[3] = (count - sixple[0]) - 1;
+					sixple[4] = (count - sixple[1]) - 1;
+					sixple[5] = (count - sixple[2]) - 1;
+					//
+					for (auto index : sixple)
+						_indices.push_back(index);
+					if (stk != 0 && ct != sliceCount) {
+						sixple[0] = counter;
+						sixple[1] = counter + sliceCount + 1;
+						sixple[2] = counter + 1;
+
+						sixple[3] = (count - sixple[0]) - 1;
+						sixple[4] = (count - sixple[1]) - 1;
+						sixple[5] = (count - sixple[2]) - 1;
+
+
+						//sixple[3] = count - sixple[0] - 1; sixple[4] = count - sixple[1] - 1; sixple[5] = sixple[4] - 1;
+						for (auto index : sixple)
+							_indices.push_back(index);
+					}
+					//
+					++counter;
+				}
+			}
+			//
+			for (int ct = 0; ct < midSliceCount - 1; ++ct) {
+				uint16_t sixple[6];
+				sixple[0] = counter;
+				sixple[1] = sixple[0] + midSliceCount;
+				sixple[2] = sixple[0] + 1;
+				//
+				sixple[3] = (count - sixple[0]) - 1;
+				sixple[4] = (count - sixple[1]) - 1;
+				sixple[5] = (count - sixple[2]) - 1;
+				for (auto index : sixple)
+					_indices.push_back(index);
+				++counter;
+			}
+		}
+	}
+	
+
+	class Sphere : public NixApplication {
+	private:
+		IArchieve*					m_arch;
+		IDriver*					m_driver;
+		IContext*					m_context;
+		IRenderPass*				m_mainRenderPass;
+		IGraphicsQueue*				m_primQueue;
+		//
+		IMaterial*					m_material;
+
+		IArgument*					m_argCommon;
+		uint32_t					m_samBase;
+		uint32_t					m_matGlobal;
+
+		IArgument*					m_argInstance;
+		uint32_t					m_matLocal;
+
+		IRenderable*				m_renderable;
+		
+		IBuffer*					m_indexBuffer;
+		uint32_t					m_indexCount;
+		IBuffer*					m_vertexBuffer;
+		//
+		IPipeline*					m_pipeline;
+
+		glm::mat4					m_model;
+		glm::mat4					m_view;
+		glm::mat4					m_projection;
+		glm::vec3					m_light;
+
+		//
+		virtual bool initialize(void* _wnd, Nix::IArchieve* _archieve ) {
+			printf("%s", "Sphere is initializing!");
+
+			HMODULE library = ::LoadLibraryA("NixVulkan.dll");
+			assert(library);
+
+			typedef IDriver*(* PFN_CREATE_DRIVER )();
+
+			PFN_CREATE_DRIVER createDriver = reinterpret_cast<PFN_CREATE_DRIVER>(::GetProcAddress(library, "createDriver"));
+
+			// \ 1. create driver
+			m_arch = _archieve;
+			m_driver = createDriver();
+			m_driver->initialize(_archieve, DeviceType::DiscreteGPU);
+			// \ 2. create context
+			m_context = m_driver->createContext(_wnd);
+			
+			m_mainRenderPass = m_context->getRenderPass();
+			m_primQueue = m_context->getGraphicsQueue(0);
+
+			RpClear clear;
+			clear.colors[0] = { 1.0, 1.0, 0.0f, 1.0f };
+			clear.depth = 1.0f;
+			clear.stencil = 1;
+			m_mainRenderPass->setClear(clear);
+			//
+			MaterialDescription mtlDesc;
+			Nix::TextReader mtlReader;
+			mtlReader.openFile(_archieve, "material/sphere.json");
+			mtlDesc.parse(mtlReader.getText());
+			RenderPassDescription rpDesc;
+			Nix::TextReader rpReader;
+			rpReader.openFile(_archieve, "renderpass/swapchain.json");
+			rpDesc.parse(rpReader.getText());
+			rpDesc.colors[0].format = m_context->swapchainColorFormat();
+			rpDesc.depthStencil.format = m_driver->selectDepthFormat(true);
+
+			TextureDescription texDesc;
+			texDesc.depth = 1;
+			texDesc.format = NixRGBA8888_UNORM;
+			texDesc.height = 64;
+			texDesc.width = 64;
+			texDesc.mipmapLevel = 1;
+			texDesc.type = Nix::TextureType::Texture2D;
+
+			bool rst = false;
+			m_material = m_context->createMaterial(mtlDesc); {
+				{ // graphics pipeline 
+					m_pipeline = m_material->createPipeline(rpDesc);
+				}
+				{ // arguments
+					m_argCommon = m_material->createArgument(0);
+					rst = m_argCommon->getUniformBlock("GlobalArgument", &m_matGlobal);
+					m_argInstance = m_material->createArgument(1);
+					rst = m_argInstance->getUniformBlock("LocalArgument", &m_matLocal);
+				}
+				{ // renderable
+					std::vector<Nix::CVertex> vertices;
+					std::vector<uint16_t> indices;
+					m_renderable = m_material->createRenderable();
+					CreateSphere(9, 16, vertices, indices);
+
+					m_vertexBuffer = m_context->createStaticVertexBuffer(vertices.data(), vertices.size() * sizeof(CVertex));
+					m_indexBuffer = m_context->createIndexBuffer(indices.data(), sizeof(uint16_t) * indices.size());
+					m_indexCount = indices.size();
+					m_renderable->setIndexBuffer( m_indexBuffer, 0);
+					m_renderable->setVertexBuffer(m_vertexBuffer, 0, 0);
+				}
+			}
+
+			Config config;
+			Nix::TextReader cfgReader;
+			cfgReader.openFile(_archieve, "config/common.json");
+			config.parse(cfgReader.getText());
+
+			m_view = glm::lookAt(
+				glm::vec3( config.eye[0], config.eye[1], config.eye[2] ), 
+				glm::vec3( config.lookat[0], config.lookat[1], config.lookat[2]),
+				glm::vec3( config.up[0], config.up[1], config.up[2])
+				);
+			m_light = glm::vec3( config.light[0], config.light[1], config.light[2] );
+
+			return true;
+		}
+
+		virtual void resize(uint32_t _width, uint32_t _height) {
+			printf("resized!");
+			m_context->resize(_width, _height);
+			Viewport vp = {
+				0.0f, 0.0f, (float)_width, (float)_height, 0.0f, 1.0f
+			};
+			Scissor ss;
+			ss.origin = { 0, 0 };
+			ss.size = { (int)_width, (int)_height };
+			//
+			m_projection = glm::perspective<float>(90, (float)_width / (float)_height, 0.01f, 200.0f);
+			//
+			m_pipeline->setViewport(vp);
+			m_pipeline->setScissor(ss);
+		}
+
+		virtual void release() {
+			printf("destroyed");	
+			m_context->release();
+		}
+
+		virtual void tick() {
+			static float angle = 0.0f;
+			angle += 0.01;
+			m_model = glm::rotate<float>(glm::mat4(), angle / 180.0f * 3.1415926, glm::vec3(0,1,0));
+			if (m_context->beginFrame()) {
+				m_mainRenderPass->begin(m_primQueue); {
+					glm::mat4x4 identity;
+					m_argCommon->setUniform(m_matGlobal, 0, &m_projection, 64);
+					m_argCommon->setUniform(m_matGlobal, 64, &m_view, 64);
+					m_argCommon->setUniform(m_matGlobal, 128, &m_light, sizeof(m_light));
+					m_argInstance->setUniform(m_matLocal, 0, &m_model, 64);
+					//
+					m_mainRenderPass->bindPipeline(m_pipeline);
+					m_mainRenderPass->bindArgument(m_argCommon);
+					m_mainRenderPass->bindArgument(m_argInstance);
+					//					
+					m_mainRenderPass->drawElements( m_renderable, 0, m_indexCount);
+				}
+				m_mainRenderPass->end();
+
+				m_context->endFrame();
+			}			
+		}
+
+		virtual const char * title() {
+			return "Sphere";
+		}
+
+		virtual uint32_t rendererType() {
+			return 0;
+		}
+	};
+}
+
+Nix::Sphere theapp;
+
+NixApplication* GetApplication() {
+    return &theapp;
+}
--- source/samples/TexEncoder/TexEncoder.cpp
+++ source/samples/TexEncoder/TexEncoder.cpp
@@ -9,6 +9,7 @@
 #include <glm/gtc/matrix_transform.hpp>
 #define STB_IMAGE_IMPLEMENTATION
 #include <stb_image.h>
+#include <TexturePacker/TexturePacker.h>
 #include <nix/io/archieve.h>
 #include <cstdio>
 #include <cassert>
@@ -19,101 +20,7 @@
 
 namespace Nix {
 
-	struct alignas(16) DXT5Block {
-		//
-		uint64_t m_alpha;
-		uint64_t m_color;
-
-		inline uint8_t calulateAlphaLevel(uint8_t _min, uint8_t _max, uint8_t _alpha) {
-			if (_min != _max) {
-				uint8_t level = ((_max - _alpha) * 0xff / (_max - _min)) >> 5;
-				// 0 1 2 3 4 5 6 7
-				// 0 2 3 4 5 6 7 1
-				static const uint8_t map[] = { 0,2,3,4,5,6,7,1};
-				return map[level];
-			} else {
-				if (_min) return 0x7;
-				else return 0x6;
-			}
-		}
-		inline uint32_t calculateRelativeWeight(uint32_t _color1, uint32_t _color2) {
-			uint32_t weight = 0;
-			for (int i = 0; i < 3; ++i) {
-				uint8_t channel1 = (_color1 >> i * 8) & 0xff;
-				uint8_t channel2 = (_color2 >> i * 8) & 0xff;
-				if (channel1 > channel2){
-					weight += channel1 - channel2;
-				} else {
-					weight += channel2 - channel1;
-				}				
-			}
-			return weight;
-		}
-		inline uint8_t calulateColorLevel(uint32_t(&_colorTable)[4], uint32_t _color) {
-			uint8_t level = 0;
-			uint32_t weight = 0xffffffff;
-			for (uint32_t i = 0; i < 4; ++i) {
-				uint32_t currentWeight = calculateRelativeWeight(_colorTable[i], _color);
-				if (weight > currentWeight) {
-					weight = currentWeight;
-					level = i;
-				}
-			}
-			return level;
-		}
-
-		void compressBitmap(uint32_t* _bitmapData, uint32_t _width, uint32_t _height) {
-			m_alpha = m_color = 0;
-			uint32_t pixelCount = _width * _height; assert(pixelCount == 16);
-
-			uint32_t alphaMin = -1;	uint32_t alphaMax = 0;
-			uint32_t redMin = -1;	uint32_t redMax = 0;
-			uint32_t greenMin = -1;	uint32_t greenMax = 0;
-			uint32_t blueMin = -1;	uint32_t blueMax = 0;
-			// find the max & min value of r g b a
-			for (uint32_t i = 0; i < pixelCount; ++i) {
-				uint32_t pixel = _bitmapData[i];
-				if (alphaMin > (pixel & 0xff000000) ) alphaMin = (pixel & 0xff000000);
-				if (alphaMax < (pixel & 0xff000000)) alphaMax = (pixel & 0xff000000);
-				if (blueMin > (pixel & 0x00ff0000)) blueMin = (pixel & 0x00ff0000);
-				if (blueMax < (pixel & 0x00ff0000)) blueMax = (pixel & 0x00ff0000);
-				if (greenMin > (pixel & 0x0000ff00)) greenMin = (pixel & 0x0000ff00);
-				if (greenMax < (pixel & 0x0000ff00)) greenMax = (pixel & 0x0000ff00);
-				if (redMin > (pixel & 0x000000ff)) redMin = (pixel & 0x000000ff);
-				if (redMax < (pixel & 0x000000ff)) redMax = (pixel & 0x000000ff);
-			}
-			alphaMin >>= 24; alphaMax >>= 24;
-			blueMin >>= 16; blueMax >>= 16;
-			greenMin >>= 8; greenMax >>= 8;
-			//redMin >>= 3; redMax >>= 3;
-			//uint64_t colorMax = redMax >> 3 | ((greenMax >> 2) << 5) | ((blueMax >> 3) << 11); // RGB565
-			//uint64_t colorMin = redMin >> 3 | ((greenMin >> 2) << 5) | ((blueMin >> 3) << 11); // RGB565
-			uint64_t colorMax = blueMax >> 3 | ((greenMax >> 2) << 5) | ((redMax >> 3) << 11); // RGB565
-			uint64_t colorMin = blueMin >> 3 | ((greenMin >> 2) << 5) | ((redMin >> 3) << 11); // RGB565
-
-			m_color |= (colorMax | colorMin << 16);
-
-			uint32_t colorTable[4] = {
-				redMax | greenMax << 8 | blueMax << 16,
-				redMin | greenMin << 8 | blueMin << 16,
-				(redMax - (redMax - redMin) / 3) | (greenMax - (greenMax - greenMin) / 3) << 8 | (blueMax - (blueMax - blueMin) / 3) << 16,
-				(redMin + (redMax - redMin) / 3) | (greenMin + (greenMax - greenMin) / 3) << 8 | (blueMin + (blueMax - blueMin) / 3) << 16,
-			};
-
-			//if (alphaMax == alphaMin) {
-//			alphaMax = 0xff;
-			//alphaMin = 0x0;
-			//}
-			m_alpha |= alphaMax;
-			m_alpha |= (alphaMin << 8);
-			// == ´¦Àí alpha / color ==
-			// 16 ¸öÏñËØ
-			for (uint32_t pixelIndex = 0; pixelIndex < 16; ++pixelIndex) {
-				m_alpha |= (uint64_t)calulateAlphaLevel(alphaMin, alphaMax, _bitmapData[pixelIndex] >> 24) << (16 + pixelIndex * 3);
-				m_color |= (uint64_t)calulateColorLevel(colorTable, _bitmapData[pixelIndex]) << (32 + pixelIndex * 2);
-			}
-		}
-	};
+	
 
 	float PlaneVertices[] = {
 		-0.5f, -0.5f, 0.0f, 0.0f, 1.0f,
@@ -125,8 +32,6 @@ namespace Nix {
 	uint16_t PlaneIndices[] = {
 		0,1,2,0,2,3
 	};
-
-	std::vector< DXT5Block > vecDXT5Data;
 
 	class TexEncoder : public NixApplication {
 	private:
@@ -158,14 +63,19 @@ namespace Nix {
 			printf("%s", "TexEncoder is initializing!");
 #ifdef NDEBUG
 			HMODULE library = ::LoadLibraryA("NixVulkan.dll");
+			HMODULE texturePackerDll = ::LoadLibraryA("TexturePacker.dll");
 #else
-			HMODULE library = ::LoadLibraryA("NixVulkand.dll");
+			HMODULE library = ::LoadLibraryA("NixVulkan.dll");
+			HMODULE texturePackerDll = ::LoadLibraryA("TexturePacker.dll");
 #endif
 			assert(library);
+			assert(texturePackerDll);
 
 			typedef IDriver*(* PFN_CREATE_DRIVER )();
 
 			PFN_CREATE_DRIVER createDriver = reinterpret_cast<PFN_CREATE_DRIVER>(::GetProcAddress(library, "createDriver"));
+
+			PFN_ENCODE_TO_DXT5 encodeToDXT5 = (PFN_ENCODE_TO_DXT5)::GetProcAddress(texturePackerDll, "EncodeToDXT5");
 
 			// \ 1. create driver
 			m_driver = createDriver();
@@ -195,26 +105,11 @@ namespace Nix {
 
 			/*FILE* file = fopen("d:/fish.png", "r");*/
 			int width, height, channel;
-			stbi_uc * bytes = stbi_load("E:/Github/NixRenderer/bin/texture/fish.png", &width, &height, &channel, 4);
+			stbi_uc * bytes = stbi_load("d:/fish.png", &width, &height, &channel, 4);
 			uint32_t * pixels = (uint32_t*)bytes;
 
-			
-
-			for (uint32_t col = 0; col < 16; ++col ) {
-				for ( uint32_t row = 0; row < 16; ++row ) {
-					std::vector<uint32_t> blockData;
-					for (uint32_t C = 0; C < 4; ++C ) {
-						for (uint32_t R = 0; R < 4; ++R) {
-							uint32_t realR = row * 4 + R;
-							uint32_t realC = col * 4 + C;
-							blockData.push_back(*(pixels + realC * 64 + realR));
-						}
-					}
-					DXT5Block block;
-					block.compressBitmap(blockData.data(), 4, 4);
-					vecDXT5Data.push_back(block);
-				}
-			}
+			std::vector<uint8_t> dxt5Bytes;
+			encodeToDXT5(pixels, width, height, dxt5Bytes);
 
 
 			TextureDescription texDesc;
@@ -238,7 +133,7 @@ namespace Nix {
 			region.offset.x = region.offset.y = region.offset.z = 0.0f;
 			region.size.depth = 1; region.size.height = 64; region.size.width = 64;
 
-			m_texture->setSubData(vecDXT5Data.data(), vecDXT5Data.size() * sizeof(DXT5Block), region);
+			m_texture->setSubData(dxt5Bytes.data(), dxt5Bytes.size(), region);
 			bool rst = false;
 			m_material = m_context->createMaterial(mtlDesc); {
 				{ // graphics pipeline 
