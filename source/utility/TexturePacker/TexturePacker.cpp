1d96a48fba32ca41342099b97d45344f696a472b
kusugawa <bhlzlx@gmail.com>
2019-07-16 02:04:12 +0800

    更新 Texture Packer 纹理打包

--- source/utility/TexturePacker/TexturePacker.cpp
+++ source/utility/TexturePacker/TexturePacker.cpp
@@ -1,40 +1,167 @@
 #include "TexturePacker.h"
 
 namespace Nix {
-
-}
-
-NIX_API_DECL void EncodeToDXT5(const uint32_t* _bitmapData, uint32_t _width, uint32_t _height, std::vector<uint8_t>& _data)
-{
-	uint32_t totalBlockCols = ((_width + 3) & ~(3)) / 4;
-	uint32_t totalBlockRows = ((_height + 3) & ~(3)) / 4;
-//	uint32_t pixelCol, pixelRow;
-	_data.resize(sizeof(Nix::DXT5Block) * totalBlockCols * totalBlockRows);
-	std::vector<uint8_t>& compressedData = _data;// (sizeof(Nix::DXT5Block) * totalBlockCols * totalBlockRows);
-	memset(compressedData.data(), 0, compressedData.size());
 	//
-	Nix::DXT5Block* block = (Nix::DXT5Block*)compressedData.data();
-	for (uint32_t blockCol = 0; blockCol < totalBlockCols; ++blockCol) {
-		for (uint32_t blockRow = 0; blockRow < totalBlockRows; ++blockRow) {
-			uint32_t bitmapBlock[16]; // 4x4
-			for (uint32_t localPixelCol = 0; localPixelCol < 4; ++localPixelCol) {
-				uint32_t pixelCol = blockCol * 4 + localPixelCol;
-				for (uint32_t localPixelRow = 0; localPixelRow < 4; ++localPixelRow) {
-					uint32_t pixelRow = localPixelRow + blockRow * 4;
+	inline TextureNode::TextureNode()
+		:x(0)
+		, y(0)
+		, width(0)
+		, height(0)
+		, flags(0)
+		, parent(nullptr)
+		, left(nullptr)
+		, right(nullptr) {
 
-					uint32_t localPixelIndex = localPixelRow + localPixelCol * 4;
-					uint32_t pixelIndex = pixelRow + pixelCol * _width;
-					//
-					if (pixelCol >= _height || pixelRow >= _width) {
-						bitmapBlock[localPixelRow + localPixelCol * 4] = bitmapBlock[0];
+	}
+
+	inline TextureNode::TextureNode(uint16_t _x, uint16_t _y, uint16_t _w, uint16_t _h, uint16_t _flags, TextureNode* _parent = nullptr )
+		:x(_x)
+		, y(_y)
+		, width(_w)
+		, height(_h)
+		, flags(_flags)
+		, parent(_parent)
+		, left(nullptr)
+		, right(nullptr)
+	{
+	}
+
+	TextureNodeFlag TextureNode::insert(uint16_t _w, uint16_t _h, uint16_t& x_, uint16_t& y_ )
+	{
+		if (this->width >= _w && this->height >= _h) {
+			if (this->width > this->height) {
+				// splite vertical
+				return this->spliteVertical(_w, _h, x_, y_);
+			} else {
+				// splite horizontal
+				return this->spliteHorizontal(_w, _h, x_, y_);
+			}
+		}
+		else {
+			return 0;
+		}
+	}
+
+	TextureNode* allocNewNode(uint16_t _x, uint16_t _y, uint16_t _w, uint16_t _h, uint16_t _flags, TextureNode* _parent ) {
+		if (_x & 1) {
+			_x = _x + 1;
+			_w = _w - 1;
+			if ((_w & ~(1)) < 4) {
+				return nullptr;
+			}
+		}
+		if (_y & 1) {
+			_y = _y + 1;
+			_h = _h - 1;
+			if ((_h & ~(1)) < 4) {
+				return nullptr;
+			}
+		}
+		return new TextureNode(_x, _y, _w, _h, _flags, _parent);
+	}
+	void freeTextureNode(TextureNode* _node)
+	{
+		delete _node;
+	}
+	bool TexturePacker::insert(const char* _key, const uint8_t* _bytes, uint32_t _length, uint16_t _width, uint16_t _height, Nix::Rect<uint16_t>& rect_)
+	{
+		InsertRetFlag flag = insert(_key, _width, _height, rect_.origin.x, rect_.origin.y);
+		if (flag & InsertSucceed) {
+			rect_.size.width = _width;
+			rect_.size.height = _height;
+			TextureRegion region;
+			region.baseLayer = m_layer;
+			region.mipLevel = 0;
+			region.offset.x = rect_.origin.x;
+			region.offset.y = rect_.origin.y;
+			region.offset.z = 0;
+			region.size.depth = 1;
+			region.size.width = rect_.size.width;
+			region.size.height = rect_.size.height;
+			m_texture->updateSubData(_key, _length, region);
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	InsertRetFlag TexturePacker::insert(const char* _key, uint16_t _width, uint16_t _height, uint16_t& x_, uint16_t& y_)
+	{
+		this->m_headNode.right;
+		std::vector<TextureNode*> nodes;
+		nodes.push_back(this->m_headNode.right);
+
+		InsertRetFlag retFlags = 0;
+		TextureNode* lastNode = nullptr;
+
+		while (nodes.size()) {
+			auto node = nodes.back();
+			nodes.pop_back();
+			if (node->flags & TextureNodeFlags::Taken) {
+				if (node->right) {
+					nodes.push_back(node->right);
+				}
+				if (node->left) {
+					nodes.push_back(node->left);
+				}
+			}
+			else {
+				retFlags = node->insert(_width, _height, x_, y_);
+				if (retFlags & InsertRetFlags::InsertSucceed) {
+					lastNode = node;
+					break;
+				}
+			}
+		}
+
+		if (retFlags & InsertRetFlags::InsertNeedClear) {
+			assert(lastNode);
+			TextureNode* nodeNeedFree = lastNode;
+			while (nodeNeedFree) {
+				if (nodeNeedFree->flags & TextureNodeFlags::LeftNode) {
+					auto parent = nodeNeedFree->parent;
+					if (parent) {
+						parent->left = nullptr;
+						freeTextureNode(nodeNeedFree);
+						if (!parent->right) {
+							nodeNeedFree = parent;
+						}
+						else {
+							break;
+						}
 					}else{
-						bitmapBlock[localPixelIndex] = ((const uint32_t*)_bitmapData)[pixelIndex];
+						break;
+					}
+				}
+				else if(nodeNeedFree->flags & TextureNodeFlags::RightNode){
+					auto parent = nodeNeedFree->parent;
+					if (parent) {
+						parent->right = nullptr;
+						freeTextureNode(nodeNeedFree);
+						if (!parent->left) {
+							nodeNeedFree = parent;
+						}
+						else {
+							break;
+						}
+					} else {
+						break;
 					}
 				}
 			}
-			block->compressBitmap(bitmapBlock, 4, 4);
-			++block;
 		}
+		if (retFlags & InsertRetFlags::InsertSucceed) {
+			m_record[_key] = {
+				{x_, y_}, 
+				{_width, _height}
+			};
+		}
+		return retFlags;
 	}
 }
 
+Nix::TexturePacker* CreateTexturePacker(Nix::ITexture* _texture, uint32_t _layer)
+{
+	Nix::TexturePacker* packer = new Nix::TexturePacker(_texture, _layer);
+	return packer;
+}
--- source/utility/TexturePacker/TexturePacker.h
+++ source/utility/TexturePacker/TexturePacker.h
@@ -1,129 +1,125 @@
 #include <NixRenderer.h>
 #include <cassert>
+#include <cstdint>
 #include <vector>
+#include <string>
+#include <map>
 
 namespace Nix {
 
-	struct alignas(16) DXT5Block {
+	enum TextureNodeFlags {
+		LeftNode = 1 << 0,
+		RightNode = 1 << 1,
+		Taken = 1 << 2
+	};
+
+	enum InsertRetFlags {
+		InsertSucceed = 1 << 0,
+		InsertNeedClear = 1 << 1
+	};
+
+	typedef uint16_t InsertRetFlag;
+	typedef uint16_t TextureNodeFlag;
+
+	class TextureNode;
+
+	NIX_API_DECL TextureNode* allocNewNode(uint16_t _x, uint16_t _y, uint16_t _w, uint16_t _h, uint16_t _flags, TextureNode* _P);
+	NIX_API_DECL void freeTextureNode(TextureNode* _node);
+
+	class NIX_API_DECL TextureNode {
+		friend class TexturePacker;
+	private:
+		// 16 bit unsigned integer is enough
+		uint16_t x;
+		uint16_t y;
 		//
-		uint64_t m_alpha;
-		uint64_t m_color;
+		uint16_t width;
+		uint16_t height;
+		//
+		uint16_t flags;
+		//
+		TextureNode* parent;
+		TextureNode* left;
+		TextureNode* right;
+		//
+	public:
+		TextureNode();
+		TextureNode(uint16_t _x, uint16_t _y, uint16_t _w, uint16_t _h, uint16_t _flags, TextureNode* _parent);
+		TextureNodeFlag insert( uint16_t _w, uint16_t _h, uint16_t& x_, uint16_t& _y );
+	private:
+		inline InsertRetFlag spliteVertical(uint16_t _w, uint16_t _h, uint16_t& x_, uint16_t& y_ ) {
+			x_ = this->x;
+			y_ = this->y;
+			//
+			uint16_t leftX = this->x;
+			uint16_t leftY = this->y + _h;
+			uint16_t leftW = _w;
+			uint16_t leftH = this->height - _h;
 
-		inline uint8_t calulateAlphaLevel(uint8_t _min, uint8_t _max, uint8_t _alpha) {
-			if (_min != _max) {
-				uint8_t level = ((_max - _alpha) * 0xff / (_max - _min)) >> 5;
-				// 0 1 2 3 4 5 6 7
-				// 0 2 3 4 5 6 7 1
-				static const uint8_t map[] = { 0,2,3,4,5,6,7,1 };
-				return map[level];
+			uint16_t rightX = this->x + _w;
+			uint16_t rightY = this->y;
+			uint16_t rightW = this->width - _w;
+			uint16_t rightH = this->height;
+			//
+			this->left = allocNewNode(leftX, leftY, leftW, leftH, TextureNodeFlags::LeftNode, this);
+			this->right = allocNewNode(rightX, rightY, rightW, rightH, TextureNodeFlags::RightNode, this);
+
+			InsertRetFlag flag = InsertRetFlags::InsertSucceed;
+
+			if (!this->left && !this->right) {
+				flag |= InsertNeedClear;
 			}
-			else {
-				return 0;
-				//if (_min) return 0x7;
-				//else return 0x6;
-			}
-		}
-		inline uint32_t calculateRelativeWeight(uint32_t _color1, uint32_t _color2) {
-			uint32_t weight = 0;
-			for (int i = 0; i < 3; ++i) {
-				uint8_t channel1 = (_color1 >> i * 8) & 0xff;
-				uint8_t channel2 = (_color2 >> i * 8) & 0xff;
-				if (channel1 > channel2) {
-					weight += channel1 - channel2;
-				}
-				else {
-					weight += channel2 - channel1;
-				}
-			}
-			return weight;
-		}
-		inline uint8_t calulateColorLevel(uint32_t(&_colorTable)[4], uint32_t _color) {
-			uint8_t level = 0;
-			uint32_t weight = 0xffffffff;
-			for (uint32_t i = 0; i < 4; ++i) {
-				uint32_t currentWeight = calculateRelativeWeight(_colorTable[i], _color);
-				if (weight > currentWeight) {
-					weight = currentWeight;
-					level = i;
-				}
-			}
-			return level;
+
+			return flag;
 		}
 
-		void compressBitmap(uint32_t* _bitmapData, uint32_t _width, uint32_t _height) {
-			m_alpha = m_color = 0;
-			uint32_t pixelCount = _width * _height; assert(pixelCount == 16);
+		inline InsertRetFlag spliteHorizontal(uint16_t _w, uint16_t _h, uint16_t& x_, uint16_t& y_) {
+			x_ = this->x;
+			y_ = this->y;
+			//
+			uint16_t leftX = this->x + _w;
+			uint16_t leftY = this->y;
+			uint16_t leftW = this->width - _w;
+			uint16_t leftH = _h;
 
-			uint32_t alphaMin = -1;	uint32_t alphaMax = 0;
-			uint32_t redMin = -1;	uint32_t redMax = 0;
-			uint32_t greenMin = -1;	uint32_t greenMax = 0;
-			uint32_t blueMin = -1;	uint32_t blueMax = 0;
-			// find the max & min value of r g b a
-			for (uint32_t i = 0; i < pixelCount; ++i) {
-				uint32_t pixel = _bitmapData[i];
-				if (alphaMin > (pixel & 0xff000000)) alphaMin = (pixel & 0xff000000);
-				if (alphaMax < (pixel & 0xff000000)) alphaMax = (pixel & 0xff000000);
-				if (blueMin > (pixel & 0x00ff0000)) blueMin = (pixel & 0x00ff0000);
-				if (blueMax < (pixel & 0x00ff0000)) blueMax = (pixel & 0x00ff0000);
-				if (greenMin > (pixel & 0x0000ff00)) greenMin = (pixel & 0x0000ff00);
-				if (greenMax < (pixel & 0x0000ff00)) greenMax = (pixel & 0x0000ff00);
-				if (redMin > (pixel & 0x000000ff)) redMin = (pixel & 0x000000ff);
-				if (redMax < (pixel & 0x000000ff)) redMax = (pixel & 0x000000ff);
+			uint16_t rightX = this->x;
+			uint16_t rightY = this->y + _h;
+			uint16_t rightW = this->width;
+			uint16_t rightH = this->height - _h;
+			//
+			this->left = allocNewNode(leftX, leftY, leftW, leftH, TextureNodeFlags::LeftNode, this);
+			this->right = allocNewNode(rightX, rightY, rightW, rightH, TextureNodeFlags::RightNode, this);
+
+			InsertRetFlag flag = InsertRetFlags::InsertSucceed;
+
+			if (!this->left && !this->right) {
+				flag |= InsertNeedClear;
 			}
-			alphaMin >>= 24; alphaMax >>= 24;
-			blueMin >>= 16; blueMax >>= 16;
-			greenMin >>= 8; greenMax >>= 8;
-			//redMin >>= 3; redMax >>= 3;
-			//uint64_t colorMax = redMax >> 3 | ((greenMax >> 2) << 5) | ((blueMax >> 3) << 11); // RGB565
-			//uint64_t colorMin = redMin >> 3 | ((greenMin >> 2) << 5) | ((blueMin >> 3) << 11); // RGB565
-			uint64_t colorMax = blueMax >> 3 | ((greenMax >> 2) << 5) | ((redMax >> 3) << 11); // RGB565
-			uint64_t colorMin = blueMin >> 3 | ((greenMin >> 2) << 5) | ((redMin >> 3) << 11); // RGB565
 
-			m_color |= (colorMax | colorMin << 16);
-
-			uint32_t colorTable[4] = {
-				redMax | greenMax << 8 | blueMax << 16,
-				redMin | greenMin << 8 | blueMin << 16,
-				(redMax - (redMax - redMin) / 3) | (greenMax - (greenMax - greenMin) / 3) << 8 | (blueMax - (blueMax - blueMin) / 3) << 16,
-				(redMin + (redMax - redMin) / 3) | (greenMin + (greenMax - greenMin) / 3) << 8 | (blueMin + (blueMax - blueMin) / 3) << 16,
-			};
-
-			//if (alphaMax == alphaMin) {
-			//			alphaMax = 0xff;
-			//alphaMin = 0x0;
-			//}
-			m_alpha |= alphaMax;
-			m_alpha |= (alphaMin << 8);
-			// == ´¦Àí alpha / color ==
-			// 16 ¸öÏñËØ
-			for (uint32_t pixelIndex = 0; pixelIndex < 16; ++pixelIndex) {
-				m_alpha |= (uint64_t)calulateAlphaLevel(alphaMin, alphaMax, _bitmapData[pixelIndex] >> 24) << (16 + pixelIndex * 3);
-				m_color |= (uint64_t)calulateColorLevel(colorTable, _bitmapData[pixelIndex]) << (32 + pixelIndex * 2);
-			}
+			return flag;
 		}
 	};
 
-
-
-// 	class NIX_API_DECL TexturePacker {
-// 	private:
-// 		ITexture*		m_texture;
-// 		uint32_t		m_layer; // if is texture 2d array
-// 	public:
-// 		void initialize( ITexture* _texture );
-// 		void release();
-// 		bool pack( uint32_t _width, uint32_t _height, const char* _rawData );
-// 	};
+	class NIX_API_DECL TexturePacker {
+	private:
+		TextureNode		m_headNode;
+		ITexture*		m_texture;
+		uint32_t		m_layer;
+		std::map< std::string, Nix::Rect<uint16_t> > m_record;
+	public:
+		TexturePacker( ITexture* _texture, uint32_t _layer ):
+			m_texture(_texture)
+			, m_layer(_layer)
+		{
+			m_headNode.left = nullptr;
+			m_headNode.right = allocNewNode(0, 0, _texture->getDesc().width, _texture->getDesc().height, TextureNodeFlags::RightNode, nullptr );
+		}
+		bool insert( const char * _key, const uint8_t* _bytes, uint32_t _length, uint16_t _width, uint16_t _height, Nix::Rect<uint16_t>& rect_ );
+		InsertRetFlag insert( const char* _key, uint16_t _width, uint16_t _height, uint16_t& x_, uint16_t& y_ );
+	};
 };
 
 extern "C" {
-
-// 	NIX_API_DECL Nix::TexturePacker* CreateTexturePacker( Nix::ITexture* _texture, uint32_t _layer );
-// 	NIX_API_DECL void DestroyTexturePacker(Nix::TexturePacker* _packer );
-// 
-// 	typedef Nix::TexturePacker* (*PFN_CREATE_TEXTURE_PACKER)(Nix::ITexture* _texture, uint32_t _layer);
-// 	typedef void (*PFN_CREATE_TEXTURE_PACKER)(Nix::TexturePacker* _packer);
-
-	NIX_API_DECL void EncodeToDXT5( const uint32_t* _bitmapData, uint32_t _width, uint32_t _height, std::vector<uint8_t>& _data );
-	typedef void (*PFN_ENCODE_TO_DXT5)(const uint32_t* _bitmapData, uint32_t _width, uint32_t _height, std::vector<uint8_t> & _data );
+	NIX_API_DECL Nix::TexturePacker* CreateTexturePacker( Nix::ITexture* _texture, uint32_t _layer );
+	typedef Nix::TexturePacker* (*PFN_CREATE_TEXTURE_PACKER)(Nix::ITexture* _texture, uint32_t _layer);
 }
