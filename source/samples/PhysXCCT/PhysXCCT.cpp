99e11d6aeb4caf7b9be19b5f2cbbf0d4c1fe27c3
bhlzlx <bhlzlx@gmail.com>
2019-07-16 14:05:53 +0800

    PxController + 一个地板，正常工作
    不知道为什么，随便一写就是正确的！之前为什么不对呢！

--- source/samples/PhysXCCT/PhysXCCT.cpp
+++ source/samples/PhysXCCT/PhysXCCT.cpp
@@ -8,382 +8,203 @@ namespace Nix {
 
 	void PhysXCCT::onMouseEvent(eMouseButton _bt, eMouseEvent _event, int _x, int _y)
 	{
-		static int lx = 0;
-		static int ly = 0;
-
-		static int dx;
-		static int dy;
-
-		switch (_event)
-		{
-		case MouseDown:
-		{
-			if (_bt == RButtonMouse)
-			{
-				lx = _x;
-				ly = _y;
-			}
-			break;
-		}
-		case MouseUp:
-		{
-			break;
-		}
-		case MouseMove:
-		{
-			if (_bt == RButtonMouse)
-			{
-				dx = _x - lx;
-				dy = _y - ly;
-				ly = _y;
-				lx = _x;
-				m_camera.RotateAxisY((float)dx / 10.0f * 3.1415f);
-				m_camera.RotateAxisX((float)dy / 10.0f * 3.1415f);
-			}
-			break;
-		}
-		}
 	}
 
 	void PhysXCCT::onKeyEvent(unsigned char _key, eKeyEvent _event)
 	{
-		if (_event == NixApplication::eKeyDown)
-		{
-			switch (_key)
-			{
-			case 'W':
-			case 'w':
-				//moveDirection = PxVec3(m_camera.GetForword().x, m_camera.GetForword().y, m_camera.GetForword().z);
-				m_camera.Forward(cammeraStepDistance);
-				break;
-			case 'a':
-			case 'A':
-				m_camera.Leftward(cammeraStepDistance);
-				break;
-			case 's':
-			case 'S':
-				m_camera.Backward(cammeraStepDistance);
-				break;
-			case 'd':
-			case 'D':
-				m_camera.Rightward(cammeraStepDistance);
-				break;
-			}
-		}
 	}
 
 
 	bool PhysXCCT::initialize(void* _wnd, Nix::IArchieve* _archieve)
 	{
-		printf("%s", "PhysXParticle is initializing!");
-
-		HMODULE library = ::LoadLibraryA("NixVulkan.dll");
-		assert(library);
-
-		typedef IDriver*(*PFN_CREATE_DRIVER)();
-
-		PFN_CREATE_DRIVER createDriver = reinterpret_cast<PFN_CREATE_DRIVER>(::GetProcAddress(library, "createDriver"));
-
-		// \ 1. create driver
-		m_driver = createDriver();
-		m_driver->initialize(_archieve, DeviceType::DiscreteGPU);
-		// \ 2. create context
-		m_context = m_driver->createContext(_wnd);
-
-		m_mainRenderPass = m_context->getRenderPass();
-		m_primQueue = m_context->getGraphicsQueue(0);
-
-		RpClear clear;
-		clear.colors[0] = { 1.0, 1.0, 0.0f, 1.0f };
-		clear.depth = 1.0f;
-		clear.stencil = 1;
-		m_mainRenderPass->setClear(clear);
+		static PxDefaultAllocator allocator;
+		static NixPhysicsErrorCallback errorCallback;
 		//
-		MaterialDescription mtlDesc;
-		Nix::TextReader mtlReader;
-		mtlReader.openFile(_archieve, "material/particle.json");
-		mtlDesc.parse(mtlReader.getText());
-		RenderPassDescription rpDesc;
-		Nix::TextReader rpReader;
-		rpReader.openFile(_archieve, "renderpass/swapchain.json");
-		rpDesc.parse(rpReader.getText());
-		rpDesc.colors[0].format = m_context->swapchainColorFormat();
-		rpDesc.depthStencil.format = m_driver->selectDepthFormat(true);
-
-		TextureDescription texDesc;
-		texDesc.depth = 1;
-		texDesc.format = NixRGBA8888_UNORM;
-		texDesc.height = 64;
-		texDesc.width = 64;
-		texDesc.mipmapLevel = 4;
-		texDesc.type = Nix::TextureType::Texture2D;
-
-		auto file = _archieve->open("texture/lightball.ktx");
-		auto mem = CreateMemoryBuffer(file->size());
-		file->read(file->size(), mem);
-		m_texture = m_context->createTextureKTX(mem->constData(), mem->size());
-		mem->release();
-		file->release();
-
-
-		bool rst = false;
-		m_material = m_context->createMaterial(mtlDesc); {
-			{ // graphics pipeline 
-				m_pipeline = m_material->createPipeline(rpDesc);
-			}
-			{ // arguments
-				m_argCommon = m_material->createArgument(0);
-				rst = m_argCommon->getUniformBlock("Argument", &m_argSlot);
-				SamplerState ss;
-				ss.min = TexFilterPoint;
-				ss.mag = TexFilterPoint;
-				ss.u = AddressModeClamp;
-				ss.v = AddressModeClamp;
-				m_argCommon->setSampler(0, ss, m_texture);
-			}
-			{ // renderable
-				m_renderable = m_material->createRenderable();
-				//m_vertexBuffer = m_context->createStaticVertexBuffer(PT_VERTICES, sizeof(PT_VERTICES));
-				m_vertexBuffer = m_context->createStaticVertexBuffer(nullptr, sizeof(glm::vec3) * 4098);
-				m_renderable->setVertexBuffer(m_vertexBuffer, 0, 0);
-			}
+		m_pxFoundation = PxCreateFoundation(PX_PHYSICS_VERSION, allocator, errorCallback);
+		m_pxPVD = PxCreatePvd(*m_pxFoundation);
+		m_transport = PxDefaultPvdSocketTransportCreate("127.0.0.1", 5425, 50);
+		m_pxPVD->connect(*m_transport, PxPvdInstrumentationFlag::eALL);
+		//
+		m_scale = PxTolerancesScale();
+		m_pxSDK = PxCreatePhysics(PX_PHYSICS_VERSION, *m_pxFoundation, m_scale, false, m_pxPVD);
+		//
+		PxCookingParams cookingParams(m_scale);
+		cookingParams.meshPreprocessParams = PxMeshPreprocessingFlags(PxMeshPreprocessingFlag::eDISABLE_CLEAN_MESH);
+		cookingParams.buildGPUData = false; //Enable GRB data being produced in cooking.
+		m_pxCooking = PxCreateCooking(PX_PHYSICS_VERSION, *m_pxFoundation, cookingParams);
+		if (!m_pxCooking) {
+			return false;
 		}
-
-		// initialize physx
-		m_timePoint = std::chrono::system_clock::now();
-		m_phySystem = new PhysXSystem();
-		m_phySystem->initialize();
-		m_phyScene = m_phySystem->createScene();
-
-		Nix::TextReader mtlHFReader;
-		mtlHFReader.openFile(_archieve, "material/heightField.json");
-		Nix::MaterialDescription mtlHFDesc;
-		mtlHFDesc.parse(mtlHFReader.getText());
-
-		m_mtlHF = m_context->createMaterial(mtlHFDesc);
-		m_pipelineHF = m_mtlHF->createPipeline(rpDesc);
-		m_argHF = m_mtlHF->createArgument(0);
+		m_pxMaterial = m_pxSDK->createMaterial(0.5f, 0.5f, 0.1f);
+		// cpu dispatcher with 2 thread
+		m_cpuDispatcher = PxDefaultCpuDispatcherCreate(2);
 
 
-		//std::string filepath = std::string(_archieve->root()) + "/texture/heightmap.png";
-		std::string filepath = std::string(_archieve->root()) + "/texture/terrian.png";
-		filepath = FormatFilePath(filepath);
-		int width, height, channel;
-		stbi_uc * bytes = stbi_load(filepath.c_str(), &width, &height, &channel, 1);
- 		m_phyScene->addHeightField(
- 			(uint8_t*)bytes,
- 			width, height,
- 			PxVec3(heightFieldOffsetX, heightFieldOffsetY, heightFieldOffsetZ),
- 			PxVec3(heightFieldXScale, heightFieldYScale, heightFieldZScale)
- 		);
 
-		hfWidth = width;
-		hfHeight = height;
-
-		// 因为我们刚刚添加了高度图，地形信息，所以我们可以立即查询一个相交点
-		// 随机两个粒子发射位置
-		// 随机一个镜头初始位置
-		static std::default_random_engine random(time(NULL));
-		std::uniform_int_distribution<int> dis1( -(int)hfWidth/4, hfWidth/4 );
-		int randX = dis1(random) * heightFieldXScale;
-		int randZ = dis1(random) * heightFieldZScale;
-		bool raycastRst = m_phyScene->raycast(PxVec3(randX, 40, randZ), PxVec3(0, -1, 0), 100, emitSource);
-		assert(raycastRst);
-		emitSource.y = emitSource.y + 1;
-
-		randX = dis1(random) * heightFieldXScale;
-		randZ = dis1(random) * heightFieldZScale;
-		raycastRst = m_phyScene->raycast(PxVec3(randX, 40, randZ), PxVec3(0, -1, 0), 100, emitSource1);
-		assert(raycastRst);
-		emitSource1.y = emitSource1.y + 1;
-
-		randX = dis1(random) * heightFieldXScale;
-		randZ = dis1(random) * heightFieldZScale;
-		PxVec3 eye;
-		raycastRst = m_phyScene->raycast(PxVec3(randX, 40, randZ), PxVec3(0, -1, 0), 100, eye);
-		assert(raycastRst);
-		eye.y = eye.y + 4;
-
-		m_camera.SetLookAt(glm::vec3(0, 0, 0));
-		m_camera.SetEye(glm::vec3( eye.x, eye.y, eye.z ));
-
-		controllerManager = m_phyScene->createControllerManager();
-		PxVec3 controllerPos;
-		randX = dis1(random) * heightFieldXScale;
-		randZ = dis1(random) * heightFieldZScale;
-		raycastRst = m_phyScene->raycast(PxVec3(randX, 40, randZ), PxVec3(0, -1, 0), 100, controllerPos);
-		assert(raycastRst);
-		controllerPos.y += 10.0f;
-
-		//player = new Player();
-		//player->initialize(controllerManager, controllerPos);
-
-		TextureDescription hfTexDesc;
-		hfTexDesc.depth = 1;
-		hfTexDesc.format = NixFormat::NixR8_UNORM;
-		hfTexDesc.width = width;
-		hfTexDesc.height = height;
-		hfTexDesc.mipmapLevel = 1;
-		hfTexDesc.type = Texture2D;
-		m_texHF = m_context->createTexture(hfTexDesc);
-		BufferImageUpload ud;
-		ud.baseMipRegion.baseLayer = 0;
-		ud.baseMipRegion.mipLevel = 0;
-		ud.baseMipRegion.offset.x = ud.baseMipRegion.offset.y = ud.baseMipRegion.offset.z = 0;
-		ud.length = sizeof(uint8_t) * width * height;
-		ud.data = bytes;
-		ud.mipDataOffsets[0] = 0;
-		ud.mipCount = 1;
-		ud.baseMipRegion.size.width = width;
-		ud.baseMipRegion.size.height = height;
-		ud.baseMipRegion.size.depth = 1;
-
-		m_texHF->uploadSubData(ud);
-		std::vector<uint16_t> indicesDataHF;// .reserve((width - 1) * 6 * height);
-		for (uint32_t r = 0; r < height - 1; ++r) {
-			for (uint32_t c = 0; c<width-1; ++c) {
-				uint16_t upleft = r * width + c;
-				uint16_t downleft = upleft + width;
-				uint16_t upright = upleft + 1;
-				uint16_t downright = downleft + 1;
-				indicesDataHF.push_back(upleft); indicesDataHF.push_back(downleft); indicesDataHF.push_back(downright);
-				indicesDataHF.push_back(upleft); indicesDataHF.push_back(downright); indicesDataHF.push_back(upright);
-			}
+		//  create scene
+		PxSceneDesc sceneDesc(m_scale);
+		sceneDesc.gravity = PxVec3(0.0f, -9.81f, 0.0f);
+		sceneDesc.cpuDispatcher = m_cpuDispatcher;
+		sceneDesc.filterShader = PxDefaultSimulationFilterShader;
+		m_pxScene = m_pxSDK->createScene(sceneDesc);
+		//m_scene->setContactModifyCallback()
+		if (!m_pxScene) {
+			return false;
 		}
-		m_indicesHF = m_context->createIndexBuffer(indicesDataHF.data(), indicesDataHF.size() * 2);
+		m_pxScenePvdClient = m_pxScene->getScenePvdClient();
+		m_pxScene->setSimulationEventCallback(&m_simulateEventCallback);
+		if (m_pxScenePvdClient) {
+			m_pxScenePvdClient->setScenePvdFlag(PxPvdSceneFlag::eTRANSMIT_CONSTRAINTS, true);
+			m_pxScenePvdClient->setScenePvdFlag(PxPvdSceneFlag::eTRANSMIT_CONTACTS, true);
+			m_pxScenePvdClient->setScenePvdFlag(PxPvdSceneFlag::eTRANSMIT_SCENEQUERIES, true);
+		}
+		m_pxMaterial = m_pxSDK->createMaterial(0.5f, 0.5f, 0.6f);
+		PxRigidStatic* groundPlane = PxCreatePlane(*m_pxSDK, PxPlane(0, 1, 0, 0), *m_pxMaterial);
+		
+		PxShape* shape = nullptr;
+		groundPlane->getShapes(&shape, sizeof(PxShape*));
+		assert(shape);
+		PxFilterData filterData;
 
-
-		SamplerState ss;
-		ss.min = TexFilterLinear;
-		ss.mag = TexFilterLinear;
-		m_argHF->setSampler(0, ss, m_texHF);
-		m_renderableHF = m_mtlHF->createRenderable();
-		m_renderableHF->setIndexBuffer(m_indicesHF, 0);
-
+		shape->setFlag(PxShapeFlag::eSCENE_QUERY_SHAPE, true);
+		shape->setFlag(PxShapeFlag::eVISUALIZATION, true);
+		shape->setSimulationFilterData(filterData);
+		m_pxScene->addActor(*groundPlane);
+		//
+		m_controllerManager = PxCreateControllerManager(*m_pxScene);
+		PxCapsuleControllerDesc capsuleDesc; {
+			capsuleDesc.climbingMode = PxCapsuleClimbingMode::eCONSTRAINED;
+			capsuleDesc.height = 1.0f;
+			capsuleDesc.radius = 0.4f;
+			capsuleDesc.behaviorCallback = &m_controllerBehaviorCallback;
+			capsuleDesc.reportCallback = &m_controllerHitReport;
+			capsuleDesc.material = m_pxMaterial;
+			capsuleDesc.nonWalkableMode = PxControllerNonWalkableMode::ePREVENT_CLIMBING;
+			capsuleDesc.position = PxExtendedVec3(0.0f, 10.0f, 0.0f);
+			capsuleDesc.userData = this;
+			//
+			capsuleDesc.contactOffset; // default by 0.1f
+			capsuleDesc.density; // default by 10.0f
+			capsuleDesc.invisibleWallHeight; // default by 0.0f
+			capsuleDesc.maxJumpHeight = 1.0f;
+		}
+		m_controller = m_controllerManager->createController(capsuleDesc);
+		//
+		m_controllerFilters.mCCTFilterCallback = nullptr;
+		m_controllerFilters.mFilterCallback = &m_controllerQueryFilterCallback;
+		m_controllerFilters.mFilterData;
+		m_controllerFilters.mFilterFlags = PxQueryFlag::eSTATIC | PxQueryFlag::ePREFILTER;
+		//
 		return true;
 	}
 
 	void PhysXCCT::resize(uint32_t _width, uint32_t _height)
 	{
-		printf("resized!");
-		m_context->resize(_width, _height);
-		Viewport vp = {
-			0.0f, 0.0f, (float)_width, (float)_height, 0.0f, 1.0f
-		};
-		Scissor ss;
-		ss.origin = { 0, 0 };
-		ss.size = { (int)_width, (int)_height };
-		m_camera.Perspective(perspectiveFOV, (float)_width / (float)_height, perspectiveNear, perspectiveFar);
-		m_pipeline->setViewport(vp);
-		m_pipelineHF->setViewport(vp);
-		m_pipeline->setScissor(ss);
-		m_pipelineHF->setScissor(ss);
-		wndWidth = _width;
-		wndHeight = _height;
 	}
 
 	void PhysXCCT::release()
 	{
-		printf("destroyed");
-		m_context->release();
+		m_pxScene->fetchResults(true);
+		//
+		m_pxCooking->release();
+		m_pxMaterial->release();
+		m_cpuDispatcher->release();
+		m_pxScene->release();
+		m_pxSDK->release();
+		m_pxPVD->release();
+		m_transport->release();
+		m_pxFoundation->release();
 	}
 
 	void PhysXCCT::tick()
 	{
 		auto now = std::chrono::system_clock::now();
-		auto duration = std::chrono::duration_cast<std::chrono::microseconds>(now - m_timePoint); 
+		auto duration = std::chrono::duration_cast<std::chrono::microseconds>(now - m_timePoint);
 		float dt = double(duration.count()) * std::chrono::microseconds::period::num / std::chrono::microseconds::period::den;
 		//dt = dt / 1000.0f;
 		if (dt > 0.1f) {
 			dt = 0.1f;
 		}
 		m_timePoint = now;
-		m_phyScene->simulate(dt);
-		static std::vector<physx::PxVec3> vertices;
-		vertices.clear();
-		m_phyScene->getParticlePrimitivePositions(vertices);
-		//if (!moveDirection.isZero()) {
-		//	player->move(moveDirection, dt);
-		//	moveDirection.x = moveDirection.y = moveDirection.z = 0;
-		//}
-		//player->tick(dt);
-		//auto playerPos = player->getPosition();
-		//m_camera.SetEye( glm::vec3(playerPos.x, playerPos.y, playerPos.z) );
+		m_pxScene->simulate(dt);
+		m_pxScene->fetchResults(true);
 
-		uint32_t pointCount = 0;
-		if ( vertices.size() < 4096) {
-			pointCount = vertices.size();
-		}
-		else {
-			pointCount = 4096;
-		}
-		static uint64_t frameCounter = 0;
-		++frameCounter;
-
-		if (frameCounter % 4 == 0) {
-			m_phyScene->addParticlePrimitive(emitSource, emiter.emit());
-			m_phyScene->addParticlePrimitive(emitSource1, emiter.emit());
-		}
-		
-
-		m_camera.Tick();
-		static uint64_t tickCounter = 0;
-		//m_phyScene
-
-		tickCounter++; 
-
-		if (m_context->beginFrame()) {
-
-			if (vertices.size()) {
-				m_vertexBuffer->setData(vertices.data(), pointCount * sizeof(physx::PxVec3), 0);
-			}		
-
-			m_mainRenderPass->begin(m_primQueue); {
-				glm::mat4x4 identity;
-
-				glm::mat4x4 viewMat = m_camera.GetViewMatrix();
-				glm::mat4x4 projMat = m_camera.GetProjMatrix();
-				glm::mat4x4 moveMat = glm::translate(identity, glm::vec3(heightFieldOffsetX, heightFieldOffsetY, heightFieldOffsetZ));
-				glm::mat4x4 scaleMat = glm::scale(identity, glm::vec3(heightFieldXScale, heightFieldYScale, heightFieldZScale));
-				glm::mat4x4 mvp = projMat * viewMat * moveMat * scaleMat;
-				//
-				m_argHF->setUniform(0, 0, &mvp, sizeof(mvp));
-				m_argHF->setUniform(0, 64, &hfWidth, 4);
-				m_argHF->setUniform(0, 68, &hfHeight, 4);
-
-				m_mainRenderPass->bindPipeline(m_pipelineHF);
-				m_mainRenderPass->bindArgument(m_argHF);
-				m_mainRenderPass->drawElements(m_renderableHF, 0, (hfWidth - 1) * (hfHeight-1) * 6);
-
-				// draw particles
-				m_argCommon->setUniform(m_argSlot, 0, &m_camera.GetViewMatrix(), 64);
-				m_argCommon->setUniform(m_argSlot, 64, &m_camera.GetProjMatrix(), 64);
-				m_argCommon->setUniform(m_argSlot, 128, &wndWidth, 4);
-				m_argCommon->setUniform(m_argSlot, 132, &wndHeight, 4);
-				m_argCommon->setUniform(m_argSlot, 136, &perspectiveNear, 4);
-				m_argCommon->setUniform(m_argSlot, 140, &perspectiveFOV, 4);
-				m_argCommon->setUniform(m_argSlot, 144, &particleSize, 4);
-				//
-				m_mainRenderPass->bindPipeline(m_pipeline);
-				m_mainRenderPass->bindArgument(m_argCommon);
-				//
-				//m_mainRenderPass->drawElements(m_renderable, 0, 6);
-				m_mainRenderPass->draw(m_renderable, 0, pointCount);
-			}
-			m_mainRenderPass->end();
-			//
-			m_context->endFrame();
-		}
+		PxVec3 disp(0.1f, -0.2f, 0.1f);
+		m_controller->move(disp, 0.01, dt, m_controllerFilters, nullptr);
 	}
 
 	const char * PhysXCCT::title()
 	{
-		return "PhysXParticle";
+		return "PhysXCCT";
 	}
 
 	uint32_t PhysXCCT::rendererType()
 	{
 		return 0;
+	}
+
+	void NixPhysicsEventCallback::onConstraintBreak(PxConstraintInfo* constraints, PxU32 count) {
+		return;
+	}
+
+	void NixPhysicsEventCallback::onWake(PxActor** actors, PxU32 count)	{
+		return;
+	}
+
+	void NixPhysicsEventCallback::onSleep(PxActor** actors, PxU32 count)	{
+		return;
+	}
+
+	void NixPhysicsEventCallback::onContact(const PxContactPairHeader& pairHeader, const PxContactPair* pairs, PxU32 nbPairs)	{
+		return;
+	}
+
+	void NixPhysicsEventCallback::onTrigger(PxTriggerPair* pairs, PxU32 count){
+		return;
+	}
+
+	void NixPhysicsEventCallback::onAdvance(const PxRigidBody*const* bodyBuffer, const PxTransform* poseBuffer, const PxU32 count){
+		return;
+	}
+
+	physx::PxControllerBehaviorFlags NixControllerBehaviorCallback::getBehaviorFlags(const PxShape& shape, const PxActor& actor)
+	{
+		return PxControllerBehaviorFlag::eCCT_CAN_RIDE_ON_OBJECT;
+	}
+
+	physx::PxControllerBehaviorFlags NixControllerBehaviorCallback::getBehaviorFlags(const PxController& controller)
+	{
+		throw std::logic_error("The method or operation is not implemented.");
+	}
+
+	physx::PxControllerBehaviorFlags NixControllerBehaviorCallback::getBehaviorFlags(const PxObstacle& obstacle)
+	{
+		throw std::logic_error("The method or operation is not implemented.");
+	}
+
+	void NixUserControllerHitReport::onShapeHit(const PxControllerShapeHit& hit)
+	{
+		return;
+	}
+
+	void NixUserControllerHitReport::onControllerHit(const PxControllersHit& hit)
+	{
+		throw std::logic_error("The method or operation is not implemented.");
+	}
+
+	void NixUserControllerHitReport::onObstacleHit(const PxControllerObstacleHit& hit)
+	{
+		throw std::logic_error("The method or operation is not implemented.");
+	}
+
+	physx::PxQueryHitType::Enum NixControllerQueryFilterCallback::preFilter(const PxFilterData& filterData, const PxShape* shape, const PxRigidActor* actor, PxHitFlags& queryFlags)
+	{
+		return PxQueryHitType::eBLOCK;
+	}
+
+	physx::PxQueryHitType::Enum NixControllerQueryFilterCallback::postFilter(const PxFilterData& filterData, const PxQueryHit& hit)
+	{
+		return PxQueryHitType::eBLOCK;
 	}
 
 }
--- source/samples/PhysXCCT/PhysXCCT.h
+++ source/samples/PhysXCCT/PhysXCCT.h
@@ -1,27 +1,89 @@
 #pragma once
 #include <NixApplication.h>
-#include <rapidjson/rapidjson.h>
-#include <rapidjson/document.h>
-#include <rapidjson/stringbuffer.h>
-#include <rapidjson/writer.h>
-#include <NixJpDecl.h>
-#include <NixRenderer.h>
-#include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
 #include <nix/io/archieve.h>
 #include <cstdio>
 #include <cassert>
-#ifdef _WIN32
-    #include <Windows.h>
-#endif
+#include <PxPhysicsAPI.h>
+#include <pvd/PxPvd.h>
 #include <chrono>
 
 // this demo does not rendering any thing!
 // only connect to `PhysX Visual Debugger` for test
 
 namespace Nix {
+
+	using namespace physx;
+
+	class NixPhysicsErrorCallback : public PxErrorCallback
+	{
+	public:
+		NixPhysicsErrorCallback() {}
+		~NixPhysicsErrorCallback() {}
+
+		virtual void reportError(PxErrorCode::Enum code, const char* message, const char* file, int line) {
+			::OutputDebugStringA(message);
+		}
+	};
+
+	class NixPhysicsEventCallback : public PxSimulationEventCallback {
+
+	public:
+		virtual void onConstraintBreak(PxConstraintInfo* constraints, PxU32 count) override;
+		virtual void onWake(PxActor** actors, PxU32 count) override;
+		virtual void onSleep(PxActor** actors, PxU32 count) override;
+		virtual void onContact(const PxContactPairHeader& pairHeader, const PxContactPair* pairs, PxU32 nbPairs) override;
+		virtual void onTrigger(PxTriggerPair* pairs, PxU32 count) override;
+		virtual void onAdvance(const PxRigidBody*const* bodyBuffer, const PxTransform* poseBuffer, const PxU32 count) override;
+	};
+
+	class NixControllerBehaviorCallback 
+		: public PxControllerBehaviorCallback 
+	{
+
+	public:
+		virtual PxControllerBehaviorFlags getBehaviorFlags(const PxShape& shape, const PxActor& actor) override;
+		virtual PxControllerBehaviorFlags getBehaviorFlags(const PxController& controller) override;
+		virtual PxControllerBehaviorFlags getBehaviorFlags(const PxObstacle& obstacle) override;
+	};
+
+	class NixUserControllerHitReport 
+		: public PxUserControllerHitReport {
+	public:
+		virtual void onShapeHit(const PxControllerShapeHit& hit) override;
+		virtual void onControllerHit(const PxControllersHit& hit) override;
+		virtual void onObstacleHit(const PxControllerObstacleHit& hit) override;
+	};
+
+	class NixControllerQueryFilterCallback 
+		: public PxQueryFilterCallback {
+	public:
+		virtual PxQueryHitType::Enum preFilter(const PxFilterData& filterData, const PxShape* shape, const PxRigidActor* actor, PxHitFlags& queryFlags) override;
+		virtual PxQueryHitType::Enum postFilter(const PxFilterData& filterData, const PxQueryHit& hit) override;
+	};
     
 	class PhysXCCT : public NixApplication {
+
+		PxFoundation*					m_pxFoundation;
+		PxPhysics*						m_pxSDK;
+		PxPvd*							m_pxPVD;
+		PxPvdTransport*					m_transport;
+		PxTolerancesScale				m_scale;
+		PxCooking*						m_pxCooking;
+		PxScene*						m_pxScene;
+		PxPvdSceneClient*				m_pxScenePvdClient;
+		PxMaterial*						m_pxMaterial;
+		PxDefaultCpuDispatcher*			m_cpuDispatcher;
+		//
+		PxControllerManager*			m_controllerManager;
+		PxController*					m_controller;
+		//
+		NixPhysicsEventCallback			m_simulateEventCallback;
+		NixControllerBehaviorCallback	m_controllerBehaviorCallback;
+		NixUserControllerHitReport		m_controllerHitReport;
+		NixControllerQueryFilterCallback\
+										m_controllerQueryFilterCallback;
+		PxControllerFilters				m_controllerFilters;
+
 	private:
 		std::chrono::time_point<std::chrono::system_clock> m_timePoint;
 		//
