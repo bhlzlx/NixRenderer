827eccb9b9ef5145c3114db362c21b822e604387
kusugawa <bhlzlx@gmail.com>
2019-07-17 02:28:40 +0800

    随便传点

--- source/samples/TextRenderer/TextRenderer.cpp
+++ source/samples/TextRenderer/TextRenderer.cpp
@@ -130,6 +130,41 @@ namespace Nix {
 				m_texturePacker->insert(std::to_string(i).c_str(), (uint8_t*)data, randW * randH * 4, randW, randH, rc);
 			}
 
+			//stbtt_fontinfo font;
+			//int i, j, ascent, baseline, ch = 0;
+			//float scale, xpos = 2; // leave a little padding in case the character extends left
+			//char* text = "Heljo World!"; // intentionally misspelled to show 'lj' brokenness
+			//IFile* fontFile = _archieve->open("font/font00.ttf"); assert(fontFile);
+			//IFile* fontBuffer = CreateMemoryBuffer(fontFile->size());
+			//fontFile->read(fontFile->size(), fontBuffer);
+			//fontFile->release();
+			//int fontCode = stbtt_InitFont(&font, (const unsigned char*)fontBuffer->constData(), 0);
+			//scale = stbtt_ScaleForPixelHeight(&font, 15);
+			//stbtt_GetFontVMetrics(&font, &ascent, 0, 0);
+			//baseline = (int)(ascent * scale);
+			//
+			//while (text[ch]) {
+			//	int advance, lsb, x0, y0, x1, y1;
+			//	float x_shift = xpos - (float)floor(xpos);
+			//	stbtt_GetCodepointHMetrics(&font, text[ch], &advance, &lsb);
+			//	stbtt_GetCodepointBitmapBoxSubpixel(&font, text[ch], scale, scale, x_shift, 0, &x0, &y0, &x1, &y1);
+			//	stbtt_MakeCodepointBitmapSubpixel(&font, &screen[baseline + y0][(int)xpos + x0], x1 - x0, y1 - y0, 79, scale, scale, x_shift, 0, text[ch]);
+			//	// note that this stomps the old data, so where character boxes overlap (e.g. 'lj') it's wrong
+			//	// because this API is really for baking character bitmaps into textures. if you want to render
+			//	// a sequence of characters, you really need to render each bitmap to a temp buffer, then
+			//	// "alpha blend" that into the working buffer
+			//	xpos += (advance * scale);
+			//	if (text[ch + 1])
+			//		xpos += scale * stbtt_GetCodepointKernAdvance(&font, text[ch], text[ch + 1]);
+			//	++ch;
+			//}
+
+			//for (j = 0; j < 20; ++j) {
+			//	for (i = 0; i < 78; ++i)
+			//		putchar(" .:ioVM@"[screen[j][i] >> 5]);
+			//	putchar('\n');
+			//}
+
 			bool rst = false;
 			m_material = m_context->createMaterial(mtlDesc); {
 				{ // graphics pipeline 
