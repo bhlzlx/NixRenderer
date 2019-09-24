ea7e593c17a423ec04432d2c6be6b181d0495679
bhlzlx <bhlzlx@gmail.com>
2019-09-25 01:57:09 +0800

    更新编译器

--- source/utility/VkShaderCompiler/VkShaderCompiler.cpp
+++ source/utility/VkShaderCompiler/VkShaderCompiler.cpp
@@ -6,9 +6,151 @@
 #include <algorithm>
 #include <utility>
 
+extern "C" {
+	Nix::IShaderCompiler* compilerInstance = nullptr;
+}
+
 namespace Nix {
-	//
-	VkShaderCompiler compiler;
+
+	class VkShaderCompiler : public IShaderCompiler, public spirv_cross::Compiler {
+	private:
+		uint32_t										m_numRef;
+		//
+		std::vector<uint32_t>							m_compiledSPV;		// binary spv data
+		std::string										m_compileMessage;	// compiling error/warnings
+		//
+		std::vector<uint16_t>							m_vecSetID;			// stores the set id
+		std::map<uint16_t, std::vector<uint16_t>>		m_bindingMap;		// set as key, binding as value
+		std::map < uint32_t, std::vector<UniformBlock::Member>>
+			m_UBOMemberInfo;	//
+//
+		std::vector<StageIOAttribute>					m_vecStageInput;
+		std::vector<StageIOAttribute>					m_vecStageOutput;
+		PushConstants									m_pushConstants;
+		//
+		std::vector<SubpassInput>						m_inputAttachment;
+		std::vector<UniformBlock>						m_vecUBO;
+		std::vector<CombinedImageSampler>				m_vecSamplers;
+		std::vector<ShaderStorageBufferObject>			m_vecSSBO;
+		std::vector<TexelBufferObject>					m_vecTBO;
+		std::vector<AtomicCounter>						m_atomicCounter;
+	private:
+		static VkShaderCompiler* Instance;
+		bool											m_initailized;
+		//
+		void addDescriptorRecord(uint16_t _id, uint16_t _binding);
+		void clearResourceInfo();
+	public:
+		VkShaderCompiler() {
+			assert(!Instance);
+			m_numRef = 0;
+			Instance = this;
+			m_initailized = false;
+		}
+
+		void retain() {
+			++m_numRef;
+		}
+		void release() {
+			--m_numRef;
+			if (!m_numRef) {
+				finalizeEnvironment();
+				delete this;
+				compilerInstance = nullptr;
+			}
+		}
+		// 初始化/清理 生产环境
+		bool initializeEnvironment();
+		void finalizeEnvironment();
+		// 将 GLSL 编译成 SPV
+		bool compile(Nix::ShaderModuleType _type, const char* _text);
+		bool getCompiledSpv(const uint32_t*& _spv, size_t& _numU32) const;
+		// 解析SPV
+		bool parseSpvLayout(Nix::ShaderModuleType _type, const uint32_t* _spv, size_t _numU32);
+		// 获取信息
+		uint16_t getDescriptorSetCount() {
+			return (uint16_t)m_vecSetID.size();
+		}
+
+		uint16_t getDescriptorSets(const uint16_t** _sets) const {
+			uint16_t c = m_vecSetID.size();
+			if (c) {
+				*_sets = m_vecSetID.data();
+			}
+			return c;
+		}
+		uint16_t getBindings(uint16_t _setID, const uint16_t** _bindings) {
+			auto it = m_bindingMap.find(_setID);
+			if (it == m_bindingMap.end()) {
+				return 0;
+			}
+			uint16_t count = it->second.size();
+			*_bindings = it->second.data();
+			return count;
+		}
+		uint16_t getUniformBlocks(const UniformBlock** _blocks) {
+			uint16_t count = m_vecUBO.size();
+			if (count) {
+				*_blocks = m_vecUBO.data();
+			}
+			return count;
+		}
+		uint16_t getUniformBlockMembers(uint16_t _set, uint16_t _binding, const UniformBlock::Member** _member) {
+			DescriptorKey key(_set, _binding);
+			auto it = m_UBOMemberInfo.find(key.val);
+			if (it == m_UBOMemberInfo.end()) {
+				return 0;
+			}
+			*_member = it->second.data();
+			return it->second.size();
+		}
+		void getPushConstants(uint16_t* _offset, uint16_t* _size) {
+			*_offset = m_pushConstants.offset;
+			*_size = m_pushConstants.size;
+		}
+		uint16_t getInputAttachment(const SubpassInput** _attachments) {
+			if (m_inputAttachment.size()) {
+				*_attachments = m_inputAttachment.data();
+			}
+			return m_inputAttachment.size();
+		}
+		uint16_t getStageInput(const StageIOAttribute** _inputs) {
+			if (m_vecStageInput.size()) {
+				*_inputs = m_vecStageInput.data();
+			}
+			return m_vecStageInput.size();
+		}
+		uint16_t getStageOutput(const StageIOAttribute** _outputs) {
+			if (m_vecStageOutput.size()) {
+				*_outputs = m_vecStageOutput.data();
+			}
+			return m_vecStageOutput.size();
+		}
+		uint16_t getShaderStorageBuffers(const ShaderStorageBufferObject** _ssbo) {
+			if (m_vecSSBO.size()) {
+				*_ssbo = m_vecSSBO.data();
+			}
+			return m_vecSSBO.size();
+		}
+		uint16_t getSamplers(const CombinedImageSampler** _samplers) {
+			if (m_vecSamplers.size()) {
+				*_samplers = m_vecSamplers.data();
+			}
+			return m_vecSamplers.size();
+		}
+		uint16_t getTexelBuffer(const TexelBufferObject** _tbo) {
+			if (m_vecTBO.size()) {
+				*_tbo = m_vecTBO.data();
+			}
+			return m_vecTBO.size();
+		}
+		uint16_t getAtomicCounter(const AtomicCounter** _counter) {
+			if (m_atomicCounter.size()) {
+				*_counter = m_atomicCounter.data();
+			}
+			return m_atomicCounter.size();
+		}
+	};
 	//
 	VkShaderCompiler* VkShaderCompiler::Instance = nullptr;
 	//
@@ -227,28 +369,27 @@ namespace Nix {
 	}
 
 	//>! 
-	bool VkShaderCompiler::parseSpvLayout(Nix::ShaderModuleType _type, uint32_t * _spv, size_t _numU32) {
+	bool VkShaderCompiler::parseSpvLayout(Nix::ShaderModuleType _type, const uint32_t * _spv, size_t _numU32) {
 		spirv_cross::Compiler spvCompiler(_spv, _numU32);
 		spirv_cross::ShaderResources spvResource = spvCompiler.get_shader_resources();
 		// get vertex binding information
 		m_vecStageInput.clear();
-		for (auto& vertexInput : spvResource.stage_inputs) {
-			auto location = spvCompiler.get_decoration(vertexInput.id, spv::Decoration::DecorationLocation);
+		for (auto& stageInput : spvResource.stage_inputs) {
+			auto location = spvCompiler.get_decoration(stageInput.id, spv::Decoration::DecorationLocation);
 			StageIOAttribute attr; 
 			attr.location = location;
-			attr.name = vertexInput.name;
-			//attr.type = vertexInput.base_type_id;
-			m_vecStageInput[location] = attr;
-			//
+			attr.name = stageInput.name;
+			m_vecStageInput.push_back(attr);
 		}
 		m_vecStageOutput.clear();
-		for (auto& vertexInput : spvResource.stage_inputs) {
-			auto location = spvCompiler.get_decoration(vertexInput.id, spv::Decoration::DecorationLocation);
+		for (auto& stageOutput : spvResource.stage_outputs) {
+			auto location = spvCompiler.get_decoration(stageOutput.id, spv::Decoration::DecorationLocation);
 			StageIOAttribute attr;
 			attr.location = location;
-			attr.name = vertexInput.name;
-			//attr.type = vertexInput.base_type_id;
-			m_vecStageOutput[location] = attr;
+			attr.name = stageOutput.name;
+			spirv_cross::SPIRType& spirType = spvCompiler.get<spirv_cross::SPIRType>(stageOutput.id);
+			auto& type = get<SPIRType>(var.basetype);
+			m_vecStageOutput.push_back(attr);
 		}
 		//
 		if (spvResource.push_constant_buffers.size()) {
@@ -353,7 +494,15 @@ namespace Nix {
 }
 
 extern "C" {
-	Nix::VkShaderCompiler * GetVkCompiler() {
-		return &Nix::compiler;
+	Nix::IShaderCompiler * GetVkShaderCompiler() {
+		if (!compilerInstance) {
+			compilerInstance = new Nix::VkShaderCompiler();
+			if (!compilerInstance->initializeEnvironment()) {
+				return nullptr;
+			}
+		}
+		Nix::VkShaderCompiler* compiler = (Nix::VkShaderCompiler*)compilerInstance;
+		compiler->retain();
+		return compilerInstance;
 	}
 }
--- source/utility/VkShaderCompiler/VkShaderCompiler.h
+++ source/utility/VkShaderCompiler/VkShaderCompiler.h
@@ -11,7 +11,6 @@ namespace Nix {
 	// 目标
 	// 1.生成SPV
 	// 2.获取SPV信息
-
 	typedef IntegerComposer<uint32_t, uint16_t> DescriptorKey;
 
 	struct Binding {
@@ -73,135 +72,35 @@ namespace Nix {
 		std::string name;
 	};
 
-
-	class NIX_API_DECL VkShaderCompiler {
+	class NIX_API_DECL IShaderCompiler {
 	private:
-		std::vector<uint32_t>							m_compiledSPV; // binary spv data
-		std::string										m_compileMessage; // compiling error/warnings
-		//
-		std::vector<uint16_t>							m_vecSetID; // stores the set id
-		std::map<uint16_t, std::vector<uint16_t>>		m_bindingMap; // set as key, binding as value
-		std::map < uint32_t, std::vector<UniformBlock::Member>> 
-														m_UBOMemberInfo; //
-		//
-		std::vector<StageIOAttribute>					m_vecStageInput;
-		std::vector<StageIOAttribute>					m_vecStageOutput;
-		PushConstants									m_pushConstants;
-		//
-		std::vector<SubpassInput>						m_inputAttachment;
-		std::vector<UniformBlock>						m_vecUBO;
-		std::vector<CombinedImageSampler>				m_vecSamplers;
-		std::vector<ShaderStorageBufferObject>			m_vecSSBO;
-		std::vector<TexelBufferObject>					m_vecTBO;
-		std::vector<AtomicCounter>						m_atomicCounter;
-	private:
-		static VkShaderCompiler* Instance;
-		bool											m_initailized;
-		//
-		void addDescriptorRecord(uint16_t _id, uint16_t _binding);
-		void clearResourceInfo();
 	public:
-		VkShaderCompiler() {
-			assert(!Instance);
-			Instance = this;
-			m_initailized = false;
-		}
-		// 初始化/清理 生产环境
-		bool initializeEnvironment();
-		void finalizeEnvironment();
+		virtual bool initializeEnvironment() = 0;
+		virtual void finalizeEnvironment() = 0;
 		// 将 GLSL 编译成 SPV
-		bool compile(Nix::ShaderModuleType _type, const char* _text);
-		bool getCompiledSpv( const uint32_t*& _spv, size_t& _numU32 ) const;
+		virtual bool compile(Nix::ShaderModuleType _type, const char* _text) = 0;
+		virtual bool getCompiledSpv(const uint32_t*& _spv, size_t& _numU32) const = 0;
 		// 解析SPV
-		bool parseSpvLayout(Nix::ShaderModuleType _type, uint32_t*  _spv, size_t _numU32);
-		// 获取信息
-		uint16_t getDescriptorSetCount() {
-			return (uint16_t)m_vecSetID.size();
-		}
-
-		uint16_t getDescriptorSets( const uint16_t** _sets ) const {
-			uint16_t c = m_vecSetID.size();
-			if (c) {
-				*_sets = m_vecSetID.data();
-			}
-			return c;
-		}
-		uint16_t getBindings( uint16_t _setID, const uint16_t** _bindings ) {
-			auto it = m_bindingMap.find(_setID);
-			if (it == m_bindingMap.end()) {
-				return 0;
-			}
-			uint16_t count = it->second.size();
-			*_bindings = it->second.data();
-			return count;
-		}
-		uint16_t getUniformBlocks(const UniformBlock** _blocks) {
-			uint16_t count = m_vecUBO.size();
-			if (count) {
-				*_blocks = m_vecUBO.data();
-			}
-			return count;
-		}
-		uint16_t getUniformBlockMembers(uint16_t _set, uint16_t _binding, const UniformBlock::Member** _member ) {
-			DescriptorKey key(_set, _binding);
-			auto it = m_UBOMemberInfo.find(key.val);
-			if (it == m_UBOMemberInfo.end()) {
-				return 0;
-			}
-			*_member = it->second.data();
-			return it->second.size();
-		}
-		void getPushConstants(uint16_t* _offset, uint16_t* _size) {
-			*_offset = m_pushConstants.offset;
-			*_size = m_pushConstants.size;
-		}
-		uint16_t getInputAttachment(const SubpassInput** _attachments) {
-			if (m_inputAttachment.size()) {
-				*_attachments = m_inputAttachment.data();
-			}
-			return m_inputAttachment.size();
-		}
-		uint16_t getStageInput(const StageIOAttribute** _inputs) {
-			if (m_vecStageInput.size()) {
-				*_inputs = m_vecStageInput.data();
-			}
-			return m_vecStageInput.size();
-		}
-		uint16_t getStageOutput(const StageIOAttribute** _outputs) {
-			if (m_vecStageOutput.size()) {
-				*_outputs = m_vecStageOutput.data();
-			}
-			return m_vecStageOutput.size();
-		}
-		uint16_t getShaderStorageBuffers(const ShaderStorageBufferObject** _ssbo) {
-			if (m_vecSSBO.size()) {
-				*_ssbo = m_vecSSBO.data();
-			}
-			return m_vecSSBO.size();
-		}
-		uint16_t getSamplers(const CombinedImageSampler** _samplers) {
-			if (m_vecSamplers.size()) {
-				*_samplers = m_vecSamplers.data();
-			}
-			return m_vecSamplers.size();
-		}
-		uint16_t getTexelBuffer(const TexelBufferObject** _tbo) {
-			if (m_vecTBO.size()) {
-				*_tbo = m_vecTBO.data();
-			}
-			return m_vecTBO.size();
-		}
-		uint16_t getAtomicCounter( const AtomicCounter** _counter ) {
-			if (m_atomicCounter.size()) {
-				*_counter = m_atomicCounter.data();
-			}
-			return m_atomicCounter.size();
-		}
+		virtual bool parseSpvLayout(Nix::ShaderModuleType _type, const uint32_t* _spv, size_t _numU32) = 0;
+		//
+		virtual uint16_t getDescriptorSets(const uint16_t** _sets) const = 0;
+		virtual uint16_t getBindings(uint16_t _setID, const uint16_t** _bindings) = 0;
+		virtual uint16_t getUniformBlocks(const UniformBlock** _blocks) = 0;
+		virtual uint16_t getUniformBlockMembers(uint16_t _set, uint16_t _binding, const UniformBlock::Member** _member) = 0;
+		virtual uint16_t getInputAttachment(const SubpassInput** _attachments) = 0;
+		virtual uint16_t getStageInput(const StageIOAttribute** _inputs) = 0;
+		virtual uint16_t getStageOutput(const StageIOAttribute** _outputs) = 0;
+		virtual uint16_t getShaderStorageBuffers(const ShaderStorageBufferObject** _ssbo) = 0;
+		virtual uint16_t getSamplers(const CombinedImageSampler** _samplers) = 0;
+		virtual uint16_t getTexelBuffer(const TexelBufferObject** _tbo) = 0;
+		virtual uint16_t getAtomicCounter(const AtomicCounter** _counter) = 0;
+		virtual void getPushConstants(uint16_t* _offset, uint16_t* _size) = 0;
+		//
+		virtual void retain() = 0;
+		virtual void release() = 0;
 	};
-
-	extern VkShaderCompiler compiler;
 }
 
 extern "C" {
-	Nix::VkShaderCompiler* GetVkCompiler();
+	NIX_API_DECL Nix::IShaderCompiler* GetVkShaderCompiler();
 }
